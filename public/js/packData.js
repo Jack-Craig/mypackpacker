b46992e5e921115ca852de7f170fab7975eafb66
unknown <jackdelcraig@gmail.com>
2021-06-12 21:59:17 -0500

    fixedd newline on main

--- public/js/packData.js
+++ public/js/packData.js
@@ -4,5 +4,7 @@ $(window).on('load', () => {
         const packId = $(event.target).attr('data-pack-id')
         $(`.section-window.${packId}-container`).addClass('hidden')
         $(`#${pageId}_${packId}`).removeClass('hidden')
+        $('.tab-header button').removeClass('folder-selected')
+        $(event.target).addClass('folder-selected')
     })    
 })
